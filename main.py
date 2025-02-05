ef05465e37e98acae35e1ac83a987669970c4c06
Behnam Dadashi <kryptickourosh@gmail.com>
2025-02-04 17:09:40 -0800

    feat: add user input

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from taylor_series import taylor_answer
-from sympy import Symbol, Function
-
+from sympy import Symbol, Function, sympify
 
 x = Symbol("x")
 y = Function("y")(x)
 
-def main():
-    # Inputs:
-    y_prime = x - y ** 2
-    a, y_a = 1, 1  # Initial conditions
-    h = 0.2  # Step size
-    n = 4  # Number of Taylor series terms
+def get_data():
+    print("-----------------------------")
+    print("Taylor series: ")
+    y_prime = input("1.Enter y'(x): ")
+    a, y_a = map(float, input("2.Enter initial conditions (a, y(a)): ").split())
+    h = float(input("3.Enter step size (h): "))
+    n = int(input("4.Enter number of Taylor series terms (n): "))
+    print("-----------------------------")
 
+    y_prime = sympify(y_prime, locals={"x" : x, "y" : y})
+    return [y_prime, a, y_a, h, n]
+
+def main():
+    y_prime, a, y_a, h, n = get_data()
     result = taylor_answer(y_prime, a, y_a, h, n)
     print(f"{result:.5f}")
 
